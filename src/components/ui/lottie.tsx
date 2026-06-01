1f351ec2e7acccdc671d434be5e746f0989ed06a
rohan-pckg <rohan.pckg@outlook.com>
2026-06-02 03:29:33 +0530

    Several improvements

--- src/components/ui/lottie.tsx
+++ src/components/ui/lottie.tsx
@@ -1,21 +1,16 @@
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react"
 
 type Props = {
-    animationData: object;
-    className?: string;
-};
+  animationData: object
+  className?: string
+}
 
-export default function LottiePlayer({
-    animationData,
-    className = "",
-}: Props) {
-    return (
-        <div className={`${className} dark:bg-zinc-100 dark:rounded-full transition-all duration-300`}>
-            <DotLottieReact
-                data={animationData}
-                loop
-                autoplay
-            />
-        </div>
-    );
+export default function LottiePlayer({ animationData, className = "" }: Props) {
+  return (
+    <div
+      className={`${className} transition-all duration-300 dark:rounded-full dark:bg-zinc-100`}
+    >
+      <DotLottieReact data={animationData} loop autoplay />
+    </div>
+  )
 }
